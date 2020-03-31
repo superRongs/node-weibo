f4519b9cdcd76834cd24e1ac1f72778e304b43f4
superRongs <rzrfreedom@163.com>
2020-03-31 11:26:12 +0800

    feat: ejs模块演练

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,35 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
-
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
+    await ctx.render('index', {
+        title: 'Hello Koa 2!',
+        name: 'qing',
+        isShow: false,
+        blogList: [{
+            id: 1,
+            content: 'aaa'
+        }, {
+            id: 2,
+            content: 'bbb'
+        }, {
+            id: 3,
+            content: 'ccc'
+        },]
+    })
 })
 
 router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
+    ctx.body = {
+        title: 'koa2 json'
+    }
+})
+
+router.get('/prefix/:username', async (ctx, next) => {
+    const { username } = ctx.params
+    ctx.body = {
+        mgs: 'this is prefix and username',
+        username
+    }
 })
 
 module.exports = router
